1bb197ca4c2ff2aad7b11ffd900c775bf0f1d955
Nikolay Lagutko <nikolay.lagutko@mail.com>
2016-02-06 17:02:19 +0300

    tests for formatters

---
+++ test/formatter-test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const jsc = require('jsverify');
+const formatters = require('../generators/app/formatters.js');
+const R = require('ramda');
+const _ = require('underscore.string');
+
+describe('formatters', function() {
+
+  it('leaves only good character in artifact name', function() {
+    var property = jsc.forall(jsc.asciistring, function(input) {
+      var result = formatters.artifactName(input);
+
+      return R.all(isArtifactName)(_.chars(result));
+    });
+
+    jsc.assert(property);
+  });
+
+  it('lowercases everything for artifact group', function() {
+    var property = jsc.forall(jsc.asciistring, function(input) {
+      var result = formatters.artifactGroup(input);
+
+      return R.all(isLowercased)(_.chars(result));
+    });
+
+    jsc.assert(property);
+  });
+
+  function isArtifactName(char) {
+    return isLetter(char) || isNumber(char) || isLowercased(char);
+  }
+
+  function isLowercased(char) {
+    return char === char.toLowerCase();
+  }
+
+  function isNumber(char) {
+    return char.match(/[0-9]/);
+  }
+
+  function isLetter(char) {
+    return char.match(/[a-z]/i);
+  }
+
+});

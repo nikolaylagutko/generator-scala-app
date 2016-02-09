7a5afe91319ba056fc94e4c13ccb5f6ebbcc89ce
Nikolay Lagutko <nikolay.lagutko@mail.com>
2016-02-09 19:41:30 +0300

    fixin' eslint errors

--- generators/app/formatters.js
+++ generators/app/formatters.js
@@ -3,25 +3,23 @@
 const _ = require('underscore.string');
 
 var Formatter = function () {
+  var words = function (source) {
+    return source.replace(/\W/g, '');
+  };
 
-  var artifactGroup = function(source) {
+  var artifactGroup = function (source) {
     return source.toLowerCase();
   };
 
-  var artifactName = function(source) {
-    return _words(_.dasherize(source));
-  };
-
-  var _words = function(source) {
-    return source.replace(/\W/g, '');
+  var artifactName = function (source) {
+    return words(_.dasherize(source));
   };
 
   return {
     artifactName: artifactName,
     artifactGroup: artifactGroup
   };
-
 };
 
-module.exports = Formatter();
+module.exports = new Formatter();
 
